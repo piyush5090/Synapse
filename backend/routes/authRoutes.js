ef58b142e9fd109923b69369ee4729fcb328253b
Piyush Govindani <govindanipiyush10@gmail.com>
2026-02-06 19:46:33 +0530

    code updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { signup, login, logout, getMe } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
